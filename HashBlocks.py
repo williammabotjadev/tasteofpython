0e7de5f75b67aafb4caa986674626f9e1cf0bb48
williammabotjadev <williammabotjadev@gmail.com>
2021-04-17 20:45:01 +0200

    Hash Blocks

---
+++ HashBlocks.py
@@ -0,0 +1,9 @@
+def main():
+    generate_blocks()
+    
+def generate_blocks():
+    for i in range(n):
+        for j in range(n):
+            print("#", end="")
+        print()
+main()
--- guessingGame.py
+++ guessingGame.py
@@ -7,14 +7,17 @@ def main():
 def guessingGame():
     number = random.randint(1, 10)
     tries = 0
-    userNumber = input("Guess the Number: ")
-    while tries < 4:
+    
+    while True:
+        userNumber = int(input("Guess the Number: "))
         if number == userNumber:
             print("You Win!")
+            break
         elif number > userNumber:
             print(userNumber, " is too low!")
         elif number < userNumber:
             print(userNumber, " is too high!")
         tries += 1
+        if(tries == 3): break 
 
 main()
